2655703cab5e0a68d742845813963cab39fbc7fe
steve-doc <stevedocherty1@gmail.com>
2023-05-22 22:45:29 +0000

    Added class for attempt objects

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 from random import randrange
 from os import system
 
+class Attempt:
+    def __init__(self, attempt, hit, miss):
+        self.attempt = attempt
+        self.hit = hit
+        self.miss = miss
+    def show(self):
+        print(f"Attempt: {self.attempt} Hit: {self.hit}  Miss: {self.miss}")
 
 def welcome_banner():
+    """
+    Prints welcome screen to console after clearing screen.
+    """
     system('clear')
     print("\n", "Welcome to\n", """  ______          __        ____                  __             __
   / ____/___  ____/ /__     / __ )________  ____ _/ /_____  _____/ /
@@ -46,14 +56,23 @@ def get_player_guess():
     return guess
 
 def make_list(data):
+    """
+    Convert code or guess into a list for easier comparison.
+    Passed a number, converts to string and then to list.
+    Returns list
+    """
     this_list = []
     string = str(data)
     this_list = [char for char in string]
     return this_list
 
 def check_answer(code, guess):
-    b = 0
-    c = 0
+    """
+    Checks each character of code for exact match then for 
+    near misses (right character in wrong place).
+    Buils lists of matches and near misses.
+    Returns length of each list.
+    """
     code_list = make_list(code)
     guess_list = make_list(guess)
     remain_code_list = code_list
@@ -62,6 +81,8 @@ def check_answer(code, guess):
 
     if guess == code:
         print("Well done, you broke the code!")
+        exact_match = len(code_list)
+        near_miss = 0
     else:
         for i in range(len(code_list) -1, -1, -1):
             # print(i)
@@ -70,9 +91,9 @@ def check_answer(code, guess):
                 match_list.append(code_list[i])
                 remain_code_list.pop(i)
                 remain_guess_list.pop(i)
-    exact_match = len(match_list)
-    remain_matches = set(remain_code_list) & set(remain_guess_list)
-    near_miss = len(remain_matches)
+        exact_match = len(match_list)
+        remain_matches = set(remain_code_list) & set(remain_guess_list)
+        near_miss = len(remain_matches)
 
     return exact_match, near_miss
 
@@ -82,8 +103,10 @@ def check_answer(code, guess):
 # code = generate_code(4)
 # guess = get_player_guess()
 
-code = 1548
-guess = 1434
+code = 1234
+guess = 4321
 
 hit, miss = check_answer(code, guess)
-print(hit, miss)
+first_attempt = Attempt(guess, hit, miss)
+print(first_attempt.attempt)
+first_attempt.show()
